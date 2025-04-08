16b6a27bcc7f304509b740c22c0f5f37cd0494b6
yasminemama19 <yasmine.mama@yele.fr>
2025-04-08 15:02:46 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -16,16 +16,19 @@ par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(df, {
   plot(Global_active_power~datetime, type="l", 
        ylab="Global Active Power (kilowatts)", xlab="", xaxt="n")
+  axis(1, at = c(df$datetime[1], df$datetime[1441], df$datetime[2880]), labels = c("Thu", "Fri", "Sat"))
   plot(Voltage~datetime, type="l", 
        ylab="Voltage (volt)", xlab="", xaxt="n")
+  axis(1, at = c(df$datetime[1], df$datetime[1441], df$datetime[2880]), labels = c("Thu", "Fri", "Sat"))
   plot(Sub_metering_1~datetime, type="l", 
        ylab="Global Active Power (kilowatts)", xlab="", xaxt="n")
   lines(Sub_metering_2~datetime,col='Red')
   lines(Sub_metering_3~datetime,col='Blue')
   legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  axis(1, at = c(df$datetime[1], df$datetime[1441], df$datetime[2880]), labels = c("Thu", "Fri", "Sat"))
   plot(Global_reactive_power~datetime, type="l", 
        ylab="Global Rective Power (kilowatts)",xlab="", xaxt="n")
-  axis(1, at = df$datetime, labels = df$day_of_week)
+  axis(1, at = c(df$datetime[1], df$datetime[1441], df$datetime[2880]), labels = c("Thu", "Fri", "Sat"))
 })
 dev.off()
