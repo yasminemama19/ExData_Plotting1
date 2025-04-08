d7cdc6a623b20cd1a701466eed43e693c81ad149
yasminemama19 <yasmine.mama@yele.fr>
2025-04-08 14:56:09 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -7,8 +7,6 @@ df <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
 df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
 df <- subset(df, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
 df$Global_reactive_power <- as.numeric(df$Global_reactive_power)
-png(file = "plot1.png", width = 480, height = 480, units = "px")
-dev.off()
 df$datetime <- paste(df$Date, df$Time)
 df$datetime <- as.POSIXct(df$datetime, format = "%Y-%m-%d %H:%M:%S")
 df$Global_reactive_power <- as.numeric(df$Global_reactive_power)
@@ -18,7 +16,7 @@ png(file = "plot3.png", width = 480, height = 480, units = "px")
 plot(df$Sub_metering_1 ~ df$datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="", xaxt="n")
 lines(df$Sub_metering_2 ~ df$datetime, col='Red')
 lines(df$Sub_metering_3 ~ df$datetime, col='Blue')
-axis(1, at = df$datetime, labels = df$day_of_week)
+axis(1, at = c(df$datetime[1], df$datetime[1441], df$datetime[2880]), labels = c("Thu", "Fri", "Sat"))
 
 # Ajouter la légende
 legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
